1162c9be01101e63a6f4cac254f6a43e5a1ab05e
SilentSherlock <1536141515@qq.com>
2024-10-17 16:55:02 +0800

    1. build tableview page

--- app/content/appManage/page.jsx
+++ app/content/appManage/page.jsx
@@ -49,8 +49,9 @@ export default function AppModule() {
             request.postJson(api.getAllTgAccount, {})
                 .then(result => {
                     if (Status.SUCCESS === result.status) {
+                        console.log("get status %d", result.status);
+                        console.log("get tgAccounts %d", result.resultMap);
                         setTgAccounts(result.resultMap.tgAccounts);
-                        console.log("get tgAccounts %d", result.resultMap.tgAccounts.length);
                         setAccountStates(new Array(result.resultMap.tgAccounts.length).fill("0"));
                     } else if (Status.FALSE === result.status) {
                         console.log("获取用户列表失败");
@@ -124,17 +125,20 @@ export default function AppModule() {
             request.postForm(api.appStart, {
                 phone: phone,
                 waitCode: code,
-                waitPassword: password,
+                waitPassword: code,
                 state: accountStates[curCardIndex]
             }).then(result => {
+                console.log("async result coming~")
                 if (Status.SUCCESS === result.status) {
                     if ("1" === result.resultMap.state) {
+                        console.log(result.description);
                         // 需要输入验证码
                         setModalTitle("WaitCode");
                         accountStates[curCardIndex] = "1";
                         setAccountStates(accountStates);
                     } else if ("2" === result.resultMap.state) {
                         // 需要输入两步验证码
+                        console.log(result.description);
                         setModalTitle("WaitPassword");
                         accountStates[curCardIndex] = "2";
                         setAccountStates(accountStates);
@@ -147,7 +151,7 @@ export default function AppModule() {
                 } else {
                     console.log("Request login failed");
                 }
-            })
+            });
         }
     }
 
